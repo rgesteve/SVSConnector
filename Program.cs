f8819bd7a75d6b5593ca04146da3f442e246c56e
R. G. Esteves <rodolfo.g.esteves@intel.com>
2024-06-11 07:53:30 -0700

    Simple test against quart server

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+	var uri = new Uri("http://localhost:5000/control/ping");
+	var client = new HttpClient();
+
+	HttpResponseMessage response =
+          await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, uri));
+
+	string content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+
+        Console.WriteLine($"Received the response {content}");
+        Console.WriteLine("Done!");
     }
 }
